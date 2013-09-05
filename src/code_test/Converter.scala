322b141637b00ea019a92dc123e885f4a2ec624f
Paul <pnikonowicz@cyrusinnovation.com>
2013-09-05 15:23:45 -0400

    got tests working. now for edge cases

--- src/code_test/Converter.scala
+++ src/code_test/Converter.scala
@@ -26,23 +26,35 @@ object Converter {
   }
 
   def stringToInt(input: String): Int = {
-    input.map(make).foldLeft(0)((acc, operation) => operation.append(acc))
-  }
-  
-  def make(input: Char): Appendable = {
-      input match {
-        case '0' => Number(0)
-        case '1' => Number(1)
-        case '2' => Number(2)
-        case '3' => Number(3)
-        case '4' => Number(4)
-        case '5' => Number(5)
-        case '6' => Number(6)
-        case '7' => Number(7)
-        case '8' => Number(8)
-        case '9' => Number(9)
-        case '+' => Symbol(+_)
-        case '-' => Symbol(-_)
-      }
+    val parsed = parseSymbol(input.iterator.next) match {
+      case Some(symbol) => input.iterator.drop(1).map(parseDecimal).toList :+ symbol
+      case None => input.map(parseDecimal).toList
     }
+    
+    parsed.foldLeft(0)((acc, appendable) => appendable.append(acc))
+  }
+
+  def parseDecimal(input: Char): Number = {
+    input match {
+      case '0' => Number(0)
+      case '1' => Number(1)
+      case '2' => Number(2)
+      case '3' => Number(3)
+      case '4' => Number(4)
+      case '5' => Number(5)
+      case '6' => Number(6)
+      case '7' => Number(7)
+      case '8' => Number(8)
+      case '9' => Number(9)
+    }
+  }
+
+  def parseSymbol(input: Char): Option[Symbol] = {
+    input match {
+      case '+' => Some(Symbol(+_))
+      case '-' => Some(Symbol(-_))
+      case _ => None
+    }
+  }
+
 }
