736f725ac40a4ebf224f98f11f596fbec00710e5
Paul <pnikonowicz@cyrusinnovation.com>
2013-09-05 19:23:40 -0400

    refactor:

--- src/convert/Converter.scala
+++ src/convert/Converter.scala
@@ -13,28 +13,24 @@ object Converter {
     parsed.foldLeft(0)((acc, appendable) => appendable.append(acc))
   }
 
-  def parseNumber(input: Char): Number = {
-    input match {
-      case '0' => Number(0)
-      case '1' => Number(1)
-      case '2' => Number(2)
-      case '3' => Number(3)
-      case '4' => Number(4)
-      case '5' => Number(5)
-      case '6' => Number(6)
-      case '7' => Number(7)
-      case '8' => Number(8)
-      case '9' => Number(9)
-      case _ => throw new ConvertException
-    }
+  def parseNumber(input: Char): Number = input match {
+    case '0' => Number(0)
+    case '1' => Number(1)
+    case '2' => Number(2)
+    case '3' => Number(3)
+    case '4' => Number(4)
+    case '5' => Number(5)
+    case '6' => Number(6)
+    case '7' => Number(7)
+    case '8' => Number(8)
+    case '9' => Number(9)
+    case _ => throw new ConvertException
   }
 
-  def parseSymbol(input: Char): Option[Symbol] = {
-    input match {
-      case '+' => Some(Symbol(+_))
-      case '-' => Some(Symbol(-_))
-      case _ => None
-    }
+  def parseSymbol(input: Char): Option[Symbol] = input match {
+    case '+' => Some(Symbol(+_))
+    case '-' => Some(Symbol(-_))
+    case _ => None
   }
 
 }
