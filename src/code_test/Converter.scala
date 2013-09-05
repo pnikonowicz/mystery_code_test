1a3d912596cf43cefb7d062b8ba3ae79fab928e3
Paul <pnikonowicz@cyrusinnovation.com>
2013-09-05 14:16:08 -0400

    put in initial arch

--- src/code_test/Converter.scala
+++ src/code_test/Converter.scala
@@ -1,7 +1,46 @@
 package code_test
 
 object Converter {
-	def stringToInt(input:String) : Int = {
-	  3
-	}
+  trait Operation {
+    def append(number: Int): Int;
+  }
+
+  case class Symbol(value: Char) extends Operation {
+    def append(number: Int): Int = {
+      3
+    }
+  }
+
+  case class Number(value: Char) extends Operation {
+    def append(number: Int): Int = {
+      shiftLeft(number) + charToDecimal(value)
+    }
+
+    def shiftLeft(input: Int): Int = {
+      input * 10
+    }
+
+    def charToDecimal(input: Char): Int = {
+      input match {
+        case '0' => 0
+        case '1' => 1
+        case '2' => 2
+        case '3' => 3
+        case '4' => 4
+        case '5' => 5
+        case '6' => 6
+        case '7' => 7
+        case '8' => 8
+        case '9' => 9
+      }
+    }
+  }
+
+  def mapToOperation(value: Char): Operation = {
+    Number(value)
+  }
+
+  def stringToInt(input: String): Int = {
+    input.map(Number).foldLeft(0)((acc, operation) => operation.append(acc))
+  }
 }
